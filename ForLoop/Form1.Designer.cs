4b689f8afd0447599c95163282b54d78c92c24bb
SangHuynh <hvsang2101@gmail.com>
2017-12-05 11:59:11 +0700

    Add project files.

---
+++ ForLoop/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ForLoop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbKieu = new System.Windows.Forms.ComboBox();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtKetQua);
+            this.groupBox2.Location = new System.Drawing.Point(16, 123);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(538, 207);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kết Quả";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(7, 27);
+            this.txtKetQua.Multiline = true;
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.ReadOnly = true;
+            this.txtKetQua.Size = new System.Drawing.Size(524, 173);
+            this.txtKetQua.TabIndex = 0;
+            // 
+            // btnXem
+            // 
+            this.btnXem.Location = new System.Drawing.Point(442, 35);
+            this.btnXem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(112, 26);
+            this.btnXem.TabIndex = 2;
+            this.btnXem.Text = "Xem";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chọn kiểu giá trị: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Nhập vào giá trị dãy số:";
+            // 
+            // cbKieu
+            // 
+            this.cbKieu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbKieu.FormattingEnabled = true;
+            this.cbKieu.Items.AddRange(new object[] {
+            "Chẵn",
+            "Lẻ"});
+            this.cbKieu.Location = new System.Drawing.Point(196, 85);
+            this.cbKieu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbKieu.Name = "cbKieu";
+            this.cbKieu.Size = new System.Drawing.Size(214, 28);
+            this.cbKieu.TabIndex = 1;
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(196, 35);
+            this.txtNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(214, 26);
+            this.txtNhap.TabIndex = 0;
+            this.txtNhap.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNhap_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnThoat);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnXem);
+            this.groupBox1.Controls.Add(this.cbKieu);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNhap);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(562, 341);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chương trình tìm các số chẵn hoặc lẻ";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(442, 85);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(112, 26);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 341);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Sử dụng ForLoop";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbKieu;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ ForLoop/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForLoop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult opt = MessageBox.Show("I will miss you", "Are you sure :<", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (opt == DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            txtKetQua.Clear();
+            if (!Int32.TryParse(txtNhap.Text, out int a) || string.IsNullOrEmpty(txtNhap.Text))
+            {
+                errorProvider1.SetError(txtNhap, "Vui lòng nhập đúng");
+            }
+            else if (string.IsNullOrEmpty(cbKieu.Text))
+            {
+                errorProvider1.SetError(cbKieu, "Vui lòng chọn kiểu");
+            }
+            else if (cbKieu.Text.Equals("Chẵn"))
+            {
+                for (int i = 0; i <= a; i+=2)
+                {
+                    txtKetQua.Text += i + "  ";
+                }
+            }
+            else if (cbKieu.Text.Equals("Lẻ"))
+            {
+                for (int i = 1; i <= a; i += 2)
+                {
+                    txtKetQua.Text += i + "  ";
+                }
+            }
+        }
+
+        private void txtNhap_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            errorProvider1.Clear();
+            //e.KeyChar != (Char)8 : khác backspace
+            //if (!Char.IsNumber(e.KeyChar) && !e.KeyChar.Equals((Char)8))
+            //Nhập nhanh giữa số và chữ sẽ xóa số??????????????????????????
+            if (!Char.IsNumber(e.KeyChar) && !e.KeyChar.Equals((Char)Keys.Back))
+            {
+                errorProvider1.SetError(txtNhap, "Vui lòng nhập đúng số");
+                e.Handled = true;
+            }
+        }
+    }
+}
